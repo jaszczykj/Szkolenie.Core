e8b313fb959c801f91bf2c4cda4c8e26aac5e759
Marcin Sulecki <marcin.sulecki@gmail.com>
2019-04-04 17:16:46 +0200

    testy

---
+++ SoftHard.Papirus.XUnitTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+
+namespace SoftHard.Papirus.XUnitTests
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData(10, 30)]
+        [InlineData(10, 70)]
+        [InlineData(10, 50)]
+        public void Test1(int x, int y)
+        {
+
+            
+
+        }
+    }
+}
