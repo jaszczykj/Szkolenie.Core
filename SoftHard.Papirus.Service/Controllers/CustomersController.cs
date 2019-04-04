3d9a85a25d682e45783cbb70693a2a73d1cf860b
Marcin Sulecki <marcin.sulecki@gmail.com>
2019-04-04 09:10:11 +0200

    Dodanie autoryzacji

--- SoftHard.Papirus.Service/Controllers/CustomersController.cs
+++ SoftHard.Papirus.Service/Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using SoftHard.Papirus.IServices;
 using SoftHard.Papirus.Models;
 using System;
@@ -9,6 +10,7 @@ using System.Threading.Tasks;
 namespace SoftHard.Papirus.Service.Controllers
 {
     [Route("api/[controller]")]
+    [Authorize]
     public class CustomersController : Controller
     {
         private readonly ICustomersService customersService;
